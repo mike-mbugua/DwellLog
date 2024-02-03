0b3a4cb106c3c659894510eab1574549600229af
michael mbugua <mikembugua960@gmail.com>
2024-02-03 20:43:02 +0300

    create user schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("user", {
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    idNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return User;
+};
