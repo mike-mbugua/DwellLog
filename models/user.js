22286be406eb79c6d01646b1cdba8e0e677afc72
michael mbugua <mikembugua960@gmail.com>
2024-02-04 22:02:57 +0300

    hashing the password

--- models/user.js
+++ models/user.js
@@ -1,24 +1,36 @@
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    firstName: {
-      type: DataTypes.STRING,
+  const User = sequelize.define(
+    "user",
+    {
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      idNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    lastName: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    idNumber: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      hooks: {
+        beforeSave: async (user) => {
+          const salt = bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        },
+      },
+    }
+  );
   return User;
 };
