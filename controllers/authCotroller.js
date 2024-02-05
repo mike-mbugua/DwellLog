5004803465f5d98bb492e8328933d2e8c86c48d0
michael mbugua <mikembugua960@gmail.com>
2024-02-05 12:45:15 +0300

    create a jwt token

--- controllers/authCotroller.js
+++ controllers/authCotroller.js
@@ -1,11 +1,23 @@
 const jwt = require("jsonwebtoken");
 const db = require("../models");
 const User = db.users;
+require("dotenv").config();
+const secretKey = process.env.JWT_SECRET;
+
+const signToken = (id) => {
+  return jwt.sign({ id }, secretKey, {
+    expiresIn: "10D",
+  });
+};
 
 exports.signup = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    return res.status(201).json({ msg: "User created successfully", user });
+    const token = signToken(user.id);
+
+    return res
+      .status(201)
+      .json({ msg: "User created successfully", token, user });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Internal server error" });
